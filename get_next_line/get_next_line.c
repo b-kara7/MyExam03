fc2b48b63151fbc199c5e53929431f4845d1fe5c
Betül Esra Kara <k.betul_98@hotmail.com>
2026-05-25 01:57:21 +0300

    rank3

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,61 +1,119 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
+/*
+ * EXERCISE: BROKEN_GNL (Get Next Line)
+ * 
+ * DESCRIPTION:
+ * Implement get_next_line that reads line by line from a file descriptor.
+ * This version may contain special cases or intentional "bugs".
+*/
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+#include "get_next_line.h"
 
-char *ft_strdup(char *s)
+char *ft_strchr(char *s, int c)
 {
-	int i = -1;
-	while (s[++i]);
-	char *a = malloc (i + 1);
-	if (!a)
-		return NULL;
-	i = -1;
-	while (s[++i])
-		a[i] = s[i];
-	a[i] = '\0';
-	return a;
+    if (!s)
+        return (NULL);
+  int i = 0;
+  while(s[i] && s[i] != (char)c)
+    i++;
+  if (s[i] == (char)c)
+    return s + i;
+  else
+    return NULL;
+}
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    if (!dest || !src)
+        return NULL;
+    size_t  i = 0;
+  while(i < n)
+  {
+      ((char *)dest)[i] = ((char *)src)[i];
+        i++;
+  }
+  return dest;
+}
+
+size_t ft_strlen(char *s)
+{
+    if (!s)
+        return 0;
+  size_t res = 0;
+  while (*s)
+  {
+    s++;
+    res++;
+  }
+  return res;
+}
+
+int str_append_mem(char **s1, char *s2, size_t size2)
+{
+  size_t size1 = ft_strlen(*s1);
+  char *tmp = malloc(size2 + size1 + 1);
+  if (!tmp)
+    return 0;
+  ft_memcpy(tmp, *s1, size1);
+  ft_memcpy(tmp + size1, s2, size2);
+  tmp[size1 + size2] = '\0';
+  free(*s1);
+  *s1 = tmp;
+  return 1; 
+}
+
+int str_append_str(char **s1, char *s2)
+{
+  return str_append_mem(s1, s2, ft_strlen(s2));
+}
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    if (dest == src)
+        return dest;
+    if (dest > src)
+    {
+        size_t  i = n;
+        while (i-- > 0)
+            ((char *)dest)[i] = ((char *)src)[i];
+    }
+    else
+    {
+        ft_memcpy(dest, src, n);
+    }
+  return dest;
 }
 
 char *get_next_line(int fd)
 {
-	static char buffer[BUFFER_SIZE];
-	static char line[70000];
-	static int buffer_pos;
-	static int buffer_read;
-	int i = 0;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return 0;
-	while (1)
-	{
-		if (buffer_pos >= buffer_read)
-		{
-			buffer_read = read(fd, buffer, BUFFER_SIZE);
-			buffer_pos = 0;
-			if (buffer_read <= 0)
-				break;
-		}
-		line[i++] = buffer[buffer_pos++];
-		if (buffer[buffer_pos - 1] == '\n')
-			break;
-	}
-	line[i] = '\0';
-	if (i == 0)
-		return NULL;
-	return (ft_strdup(line));
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-
-int main()
-{
-	int fd = open("ilyas", O_CREAT | O_RDWR , 777);
-	printf("%s", get_next_line(fd));
+  static char b[BUFFER_SIZE + 1] = "";
+  char *ret = malloc(1);
+  if (!ret)
+    return NULL;
+  ret[0] = 0;
+  char *tmp = ft_strchr(b, '\n');
+  while(!tmp)
+  {
+    if (!str_append_str(&ret, b))
+      return (free(ret), NULL);
+    int read_ret = read(fd, b, BUFFER_SIZE);
+    if (read_ret == -1)
+      return (free(ret), NULL);
+    if (read_ret == 0)
+    {
+        b[0] = 0;
+        if (*ret)
+            return ret;
+        free(ret);
+        return NULL;
+    }
+    b[read_ret] = 0;
+    tmp = ft_strchr(b, '\n');
+  }
+  if (!str_append_mem(&ret, b, tmp - b + 1))
+  {
+    free(ret);
+    return NULL;
+  }
+  ft_memmove(b, tmp + 1, ft_strlen(tmp + 1) + 1);
+  return ret;
 }
